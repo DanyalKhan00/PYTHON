60f793da94391212d43de3eb5b4ea29bf0785e33
DanyalKhan00 <ukt16294@email.com>
2026-07-07 07:17:49 -0700

    Added first.py and second.py

--- third.py
+++
@@ -1,9 +0,0 @@
-"""x=int(input("Enter No"))
-y=int(input("Enter No"))
-def addition(x,y):
-print("Addition =",x+y)
-addition(x,y)
-print("Addition =",x+y)
-addition(x,y)"""
-print("hello Danyal")
-print("This is me danyal khan")
