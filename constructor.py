7b591f85d2e76dad180997a88c1b90057c020d89
DanyalKhan00 <ukt16294@email.com>
2026-07-19 00:30:23 -0700

    Constructor

---
+++ constructor.py
@@ -0,0 +1,73 @@
+# ***** ALL ABOUT CONSTRUCTOR ****
+#Q1: Movie Ticket Booking System
+
+# def new_func():
+#     class movie_ticket:
+#         def __init__(self,cus_name,movie_name,ticket):
+#             self.cus_name = cus_name
+#             self.    movie_name
+#             self.ticket = ticket
+#         def bill(self):
+#             total = self.ticket * 800
+#             if self.ticket > 5:
+#                 discount  = total * 0.10
+#             else:
+#                 discount = 0
+#             final_bill = total - discount 
+#             print("Customer Name :", self.cus_name)
+#             print("Movie Name : ", self.         
+#             print("No Of Ticket : ", self.ticket)
+#             print("Total Bill : ", total)
+#             print("Discount: ", discount)
+#             print("Final bill : ", final_bill)
+#     m1=movie_ticket("Dani", "Titanic",8)
+#     m1.bill()
+
+# new_func()
+
+                  
+#Q2:
+# Laptop Store Inventory
+# class laptop:
+#     def __init__(self,brand,ram,price):
+#         self.brand = brand
+#         self.ram = ram
+#         self.price = price
+
+#     def upgrade_ram(self,extra_ram):
+#         self.ram +=extra_ram
+    
+#     def increase_price(self,amount):
+#         self.price += amount
+
+#     def disp(self):
+#         print("Brand : " , self.brand)
+#         print(" RAM : " , self.ram)
+#         print(" Price : ",self.price)
+# l=laptop("Dell",8,80000)
+# l.upgrade_ram(8)
+# l.increase_price(9000)
+# l.disp()
+
+#Q3: ***    Gym Membership System   ***
+# class Gym_member:
+#     def __init__(self,name,membership,fees):
+#         self.name = name
+#         self.membership = membership
+#         self.fees = fees
+#     def yearly_fees(self):
+#         total = self.fees * 12
+#         if self.membership.lower() == "gold":
+#             discount = total * 0.15
+
+#         else:
+#             discount = 0
+#         finall_bill =  total - discount
+#         print("Name : " , self.name)
+#         print("Member_Ship : ", self.membership)
+#         print("Yearly fees : ",total)
+#         print("discount : ",discount)
+#         print("Finall _bill : ",finall_bill)
+# g=Gym_member("Dani","Gold",3000)
+# g.yearly_fees()
+              
